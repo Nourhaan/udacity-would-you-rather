17d8f7f013739961915649ccf36734aa31da357d
unknown <nourhanabdelraheem@gmail.com>
2021-07-18 22:54:06 +0200

    commented import bootstrap

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.min.js';
+// import 'bootstrap/dist/css/bootstrap.min.css';
+// import 'bootstrap/dist/js/bootstrap.min.js';
 import 'react-jquery-plugin'
 import 'font-awesome/css/font-awesome.min.css';
 import { Provider } from 'react-redux'
